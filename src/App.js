58ae68704c4bc7d030b29eeae035fbd5f858bf3b
Ricky Chan <ric0611@gmail.com>
2020-02-21 14:47:01 -0800

    add spring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 import './App.css';
+import { useSpring, animated, config } from 'react-spring'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
 
-function App() {
+function App({ parallax }) {
+  // const props = useSpring({
+  //   config: config.gentle,
+  //   opacity: 1, 
+  //   from: {opacity: 0}
+  // })
+  const [toggle, setToggle] = useState(false)
+  const props = useSpring({
+    to: [{ opacity: 1, color: '#ffaaee' }, { opacity: .5, color: 'green' }],
+    from: { opacity: 0, color: 'red' }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Parallax pages={3}>
+
+        <ParallaxLayer offset={0} speed={1} style={{ backgroundColor: 'black' }} />
+        
+        {/* <ParallaxLayer  */}
+          offset={0} 
+          speed={0}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          >
+          <animated.div style={{color: 'white', width: '80%'}}>
+            <h1 >Hi, I am Ricky, Hi, I am Ricky, web developer, grahic designer</h1>
+          </animated.div>
+        {/* </ParallaxLayer> */}
+
+        <ParallaxLayer offset={1} speed={0.5}>
+          <span onClick={() => parallax.scrollTo(1)}>Layers can contain anything</span>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+
+
+
+        <ParallaxLayer offset={2} speed={0.5}>
+          <animated.div style={props}>I will fade in and out</animated.div>
+        </ParallaxLayer>
+
+        
+
+      </Parallax>
+
     </div>
   );
 }
