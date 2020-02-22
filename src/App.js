e8f923d35d932fb2c56a3d0f061f46427ac26dd6
Ricky Chan <ric0611@gmail.com>
2020-02-22 03:42:35 -0800

    create landing page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import './App.css';
-import { useSpring, animated, config } from 'react-spring'
+import background from './images/background.png'
+import building from './images/building.png'
+import buildingBW from './images/building-BW.png'
+import bokeh1 from './images/bokeh1.png'
+import bokeh2 from './images/bokeh2.png'
+
+import { useTransition, useSpring, animated, config } from 'react-spring'
+
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
 import Box from '@material-ui/core/Box';
 
 import { makeStyles } from '@material-ui/core/styles';
-import background from './images/background.jpg'
 import Typography from '@material-ui/core/Typography';
-import 'typeface-montserrat';
 
 const useStyles = makeStyles({
   root: {
@@ -24,48 +29,119 @@ const useStyles = makeStyles({
 });
 
 function App() {
-  const props = useSpring({
-    to: [{ opacity: 1, color: '#ffaaee' }, { opacity: .5, color: 'green' }],
-    from: { opacity: 0, color: 'red' }
+  const [toggle, setToggle] = useState(true)
+  const [toggle2, setToggle2] = useState(true)
+
+
+
+  const light = useSpring({
+    opacity: toggle ? 1 : 0,
+    // onRest:(()=>{
+    //   setToggle(!toggle)
+    // })
   })
+
+  const lightTranslate = useSpring({
+    transform: toggle2 ? 'translate3d(1px,1px,1px)' : 'translate3d(2px,2px,1px)',
+    duration: 100,
+    velocity: 3,
+    tension: 1000,
+    onRest:(()=>{
+      setToggle2(!toggle2)
+    })
+  })
+
 
   return (
   <Typography component="div">
-      <Parallax pages={3}
+    
+      <Parallax pages={2}
       style={{ 
         backgroundColor: 'black', 
         backgroundImage: `url(${background})`,   
         backgroundSize: 'auto 100%',
         backgroundRepeat: 'no-repeat',
-        // backgroundAttachment: 'fixed',
+        backgroundAttachment: 'fixed',
         backgroundPosition: 'right top',
       }}>
-        
+
+
+         {/* my name   */}
         <ParallaxLayer
           offset={0} 
-          speed={0}
+          speed={-.5}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
           >
           <animated.div style={{
             color: 'white', width: '80%'}}>
-          <Box fontWeight="fontWeightBold" fontSize={100} fontFamily="Monospace">
-            Ricky Chan
-          </Box>
-
-          <h2 >Web developer &<br/> Grahpic Designer</h2>
-          <h3 >from physical to digital</h3>
+            <Box fontWeight="fontWeightBold" fontSize={70} letterSpacing={-4} lineHeight={1}>
+              Hi, <br/>I'm Ricky
+            </Box>
+            <Box fontWeight="fontWeightLight" fontSize={17} letterSpacing={5} mt={2}>
+            Full Stack Developer  &   <br/>Grahpic Designer
+            </Box>
+            <Box fontWeight="fontWeightLight" fontSize={12} letterSpacing={2} mt={2}>
+            from physical to digital @ Vancouver
+            </Box>
           </animated.div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={0.5}>
+
+        {/* Building */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          style={{ 
+            width: '100%',
+            backgroundImage: `url(${buildingBW})`,   
+            backgroundSize: 'auto 100%',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right top',
+            // marginLeft: '11px'
+          }}>
+            
+          </ParallaxLayer>
+
+        {/* light */}
+        <animated.div style={light}>
+
+          <ParallaxLayer
+          offset={0}
+          speed={0.15}
+          style={{ 
+            width: '100%',
+            backgroundImage: `url(${bokeh1})`,   
+            backgroundSize: 'auto 100%',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right top',
+            // marginLeft: '11px',
+            mixBlendMode: 'color-dodge',
+          }}>
+          <animated.div style={lightTranslate}>
+              {/* <img src={bokeh1} 
+              style={{
+                 height: '100%',
+                 position: 'relative',
+                  right: '0px',
+                  bottom: '0px',
+                 }} />  */}
+          </animated.div>
+          </ParallaxLayer>
+
+          </animated.div>
+         
+
+        <ParallaxLayer offset={0.999} speed={.3} style={{ backgroundColor: 'black' }} />
+
+        {/* <ParallaxLayer offset={1} speed={0.5}>
           <span onClick={() => props.parallax.scrollTo(1)}>Layers can contain anything</span>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+        
 
         <ParallaxLayer offset={2} speed={0.5}>
           <animated.div style={props}>I will fade in and out</animated.div>
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
       </Parallax>
     </Typography>
