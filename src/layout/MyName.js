5f08c390d966c31570535492d116a08a55efc385
rickychan0611 <ric0611@gmail.com>
2024-06-10 16:02:36 -0700

    fix typo

--- src/layout/MyName.js
+++ src/layout/MyName.js
@@ -22,7 +22,8 @@ const MyName = () => {
       </Typography>
       <Box mt={3}>
         <Typography variant='h3'>
-          Front-end Developer & <br />Graphic Designer
+          Front-end Developer & <br />
+          Graphic Designer
       </Typography>
       </Box>
       <Typography variant='overline'>
